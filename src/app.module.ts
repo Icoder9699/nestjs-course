f1d8fc3de5d147a0b31edbebff7d7fc2ed547363
Azizbek Karimjonov <nasiya@Noutbuk-NASIYA.local>
2024-01-31 10:11:53 +0500

    add: tag service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TagsModule } from './tags/tags.module';
+import { TagModule } from './tags/tag.module';
 
 @Module({
-  imports: [TagsModule],
+  imports: [TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tags/tags.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('tags')
-export class TagsController {
-  @Get()
-  findAll() {
-    return ['a', 'b', 'c'];
-  }
-}
