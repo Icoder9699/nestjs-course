510d5c5cfe4e8a2200a6f4940009cb284aa08297
Azizbek <akarimdjanov@paymart.uz>
2024-06-30 12:40:29 +0500

    users repository finished

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  createUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(createUserDto: CreateUserDto) {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    return await this.userRepository.save(newUser);
   }
 }
